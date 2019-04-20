d65bf51cfca8c04d365ee97cf1ebc32eaa203d64
vv13 <zwhvv13@gmail.com>
2019-04-20 15:00:47 +0800

    feat: add element

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <el-form label-position="left" label-width="80px" :model="form">
+      <el-form-item label="账号">
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+};
+</script>
---
+++ src/views/Main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main">
+    <h1>This is an Main page</h1>
+  </div>
+</template>
