add5b1dfbc6e83a63288f9f7c28d21469d5c1230
vv13 <zwhvv13@gmail.com>
2019-04-21 13:44:46 +0800

    style: question style

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,37 +1,40 @@
 <template>
-  <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="题目" required>
-      <el-input v-model="form.title" size="small"></el-input>
-    </el-form-item>
-    <el-form-item
-      :label="item.label"
-      required
-      v-for="item in form.options"
-      :key="item.index"
-    >
-      <el-row :gutter="16">
-        <el-col :span="12">
-          <el-input v-model="item.text"></el-input>
-        </el-col>
-        <el-col :span="11">
-          <el-checkbox v-model="item.correct"></el-checkbox> 正确答案
-        </el-col>
-      </el-row>
-    </el-form-item>
-    <el-form-item label="分数">
-      <el-select v-model="form.score">
-        <el-option value="1"></el-option>
-        <el-option value="2"></el-option>
-        <el-option value="3"></el-option>
-        <el-option value="4"></el-option>
-        <el-option value="5"></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">立即创建</el-button>
-      <el-button @click="onCancel">取消</el-button>
-    </el-form-item>
-  </el-form>
+  <el-card class="question-card">
+    <h2>创建题目</h2>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="题目" required>
+        <el-input v-model="form.title" size="small"></el-input>
+      </el-form-item>
+      <el-form-item
+        :label="item.label"
+        required
+        v-for="item in form.options"
+        :key="item.index"
+      >
+        <el-row :gutter="16">
+          <el-col :span="12">
+            <el-input v-model="item.text"></el-input>
+          </el-col>
+          <el-col :span="11">
+            <el-checkbox v-model="item.correct"></el-checkbox> 正确答案
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item label="分数">
+        <el-select v-model="form.score">
+          <el-option value="1"></el-option>
+          <el-option value="2"></el-option>
+          <el-option value="3"></el-option>
+          <el-option value="4"></el-option>
+          <el-option value="5"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item class="question__footer">
+        <el-button @click="onCancel">取消</el-button>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
@@ -93,3 +96,13 @@ export default {
   },
 };
 </script>
+
+<style lang="less">
+.question-card {
+  width: 600px;
+  margin: auto;
+}
+.question__footer {
+  float: right;
+}
+</style>
